924eb1e9f35b12e2b83b50195c43981cd3acba0f
THANAPAT SITTIPORNCHAISAKUL <oatkung.ts@gmail.com>
2021-11-29 23:41:08 +0700

    downn

--- Testdownload.py
+++ Testdownload.py
@@ -1,20 +1,36 @@
 import youtube_dl
+from youtube_dl import downloader
 def run():
-    video_url = input("please enter youtube video url:")
+    video_url = input("please enter youtube video url: ")
     video_info = youtube_dl.YoutubeDL().extract_info(
         url = video_url,download=False
     )
     filename = f"{video_info['title']}.mp3"
+    
     options={
         'format':'bestaudio/best',
         'keepvideo':False,
-        'outtmpl':filename,
+        'outtmpl':f'archive/song/{filename}',
     }
 
     with youtube_dl.YoutubeDL(options) as ydl:
+        ydl._ies = [ydl.get_info_extractor('Youtube')]
         ydl.download([video_info['webpage_url']])
-
-    print("Download complete... {}".format(filename))
-
+    
+    print("Download complete... {}".format(filename))   
+   
+    g = open("archive/song/yoursongpath.txt", "r+",encoding='utf-8')
+    Write = True
+    for i in g:                 
+        if filename in i:
+            Write = False
+            break  
+    g.close()      
+    #write
+    if Write: 
+        f = open("archive/song/yoursongpath.txt", "a",encoding='utf8')  
+        f.write(f'archive/song/{filename}\n')
+        f.close()
+    
 if __name__=='__main__':
     run()
