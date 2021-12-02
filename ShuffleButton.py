d8f525c510d62d5e4af0c3b547040b9a8fc8824d
HaNiBaHei <40122714+HaNiBaHei@users.noreply.github.com>
2021-12-02 20:44:48 +0700

    แก้ไม้โมก

--- ShuffleButton.py
+++ ShuffleButton.py
@@ -8,17 +8,17 @@ class ShuffleButton(MDIconButton,HoverBehavior,ToggleButtonBehavior):
         if self.state is 'normal':
             self.text_color = (1,1,1,1)
         else:
-            self.text_color = (1,0.78,0.85,1)
+            self.text_color = (1,0.60,0.75,1)           #[1,0.41,0.69,1]
         return super().on_enter()
     def on_leave(self):
         if self.state is 'normal':
-            self.text_color = (0.7,0.7,0.7)
+            self.text_color = (0.7,0.7,0.7,1)
         else:
-            self.text_color = (1,0.73,0.8,1)
+            self.text_color = (1,0.41,0.69,1)
         return super().on_leave()
     def on_release(self):
         if self.state is 'normal':
-            self.text_color = (0.7,0.7,0.7)
+            self.text_color = (0.7,0.7,0.7,1)
         else:
             self.text_color = (1,0.73,0.8,1)
         return super().on_release()
