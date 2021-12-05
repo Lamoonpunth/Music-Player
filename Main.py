f9055fe0f2881ae107b4311cc46409d7658c9843
maimoke <maimokemm@hotmail.com>
2021-12-06 00:56:30 +0700

    slidingwindow search

--- Main.py
+++ Main.py
@@ -286,12 +286,20 @@ class MainGridLayout(Widget):
     # Search song in Playlist(ค้นหาเพลงในเพลย์ลิสต์)
     def Searched_Song(self, text="", search=False):
         # print(f'{type(yoursong)}')             
-        self.searchedPlaylist.clearSong()            
+        self.searchedPlaylist.clearSong()
         ListofSong =[]
         ListofSim =[]
         temp = None
         for songg in self.playlistlist[self.playlistindex].playlist:
-            val = nltk.edit_distance(text.casefold(),songg.name.casefold())       
+            #slidingwindow
+            if range(len(songg.name.casefold())<len(text.casefold())):
+                val=999
+            else:
+                val=999
+                for i in range(len(songg.name.casefold())-len(text.casefold())):
+                    temp = nltk.edit_distance(text.casefold(),songg.name.casefold()[i:i+len(text.casefold())])
+                    if temp<val:
+                        val=temp
             ListofSong.append(songg)
             ListofSim.append(val)         
                  
