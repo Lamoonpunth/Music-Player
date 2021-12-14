4e198c30e1bc6425ad764b916d014b123ed00bd5
THANAPAT SITTIPORNCHAISAKUL <oatkung.ts@gmail.com>
2021-12-15 00:04:48 +0700

    gioioooooo

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-def EnterPlaylistName(self,name):
-    #     f = open("archive/song/playlist.txt", "r+",encoding='utf-8')
-    #     for x in f:
-    #         continue
-    #     f.write("%"+name+"\n")
-    #     f.close()
-    #     self.playlistlist.append(playlist(name))
-    #     self.updateplaylistfile()
-    #     self.showplaylist(self.playlistlist)
